452bd3088f8d4c155bb31fafdad36bab7c472c14
cryptobr <luizguilhermemmj@gmail.com>
2019-07-19 09:01:34 -0300

    Update resize_linux_partition.sh

--- resize_linux_partition.sh
+++ resize_linux_partition.sh
@@ -4,7 +4,7 @@
 # @f1r3m4n 2019
 # Expand volumes for T2 Linux instance
 # Reference: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/recognize-expanded-volume-linux.html
-# Do not forget to install this package -> $ yum install cloud-utils-growpart
+# Do not forget to install this package -> $ yum install cloud-utils-growpart xfsprogs
 
 echo "Volumes Instances"
 echo ""
@@ -18,7 +18,10 @@ echo "Expand partition..."
 sleep 1
 echo ""
 sudo growpart /dev/$NAME $NUMBER
+# For ext2, ext3 and ext4
 sudo resize2fs /dev/$NAME$NUMBER
+# For XFS
+sudo xfs_growfs /dev/$NAME$NUMBER
 echo ""
 echo "Show new size partition"
 df -h
