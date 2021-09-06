67f907221ac39577a799d679401b135288fc3baa
cryptobr <luizguilhermemmj@gmail.com>
2021-09-06 11:57:22 -0300

    Update getValueWithFilter.py

--- dynamoDB/getValueWithFilter.py
+++ dynamoDB/getValueWithFilter.py
@@ -1,9 +1,7 @@
 import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
-from datetime import date
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+
 client = boto3.resource('dynamodb')
 table = client.Table("YouTable")
 
